ee04cdd20500de2315b4c656d25b138f4218412b
m v <morgane.voisin@e-artsup.net>
2023-03-11 13:10:07 +0100

    Début du menu et credit

---
+++ Assets/Scripts/MenuScripts/MenuController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    bool Pressed = false;
+
+    private void Start()
+    {
+       
+    }
+
+    public void ChangeScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ReloadLvl(string _sceneName)
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(_sceneName);
+    }
+    
+}
