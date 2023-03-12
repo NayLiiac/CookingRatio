43f0750ec5ad2147fc1dfbf062b398a1629afb34
NayLiiac <levoyer.kylian24@gmail.com>
2023-03-12 14:46:59 +0100

    Prefab Correction

--- Assets/Scripts/CriticScripts/RecipesCounter.cs
+++ Assets/Scripts/CriticScripts/RecipesCounter.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class RecipesCounter : MonoBehaviour
 {
     public int scoreValue = 0;
     public TextMeshProUGUI score;
+    int scoreMax = 48;
 
     void Start()
     {
-        score.text = scoreValue.ToString();
+        score.text = scoreValue.ToString() + " / " + scoreMax;
     }
     
     public void AddRecipe()
     {
         scoreValue++;
-        score.text = scoreValue.ToString();
+        score.text = scoreValue.ToString() + " / " + scoreMax;
+
+        if (scoreValue == scoreMax)
+        {
+            SceneManager.LoadScene("End");
+        }
     }
     
 }
