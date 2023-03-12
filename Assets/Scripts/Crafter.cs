acefc310aa11be40f220b2a51a45fa130bffbea6
Yoann | Finiarael <yoann.lemetayer@e-artsup.net>
2023-03-12 02:01:48 +0100

    Début dico images

--- Assets/Scripts/Crafter.cs
+++ Assets/Scripts/Crafter.cs
@@ -18,68 +18,81 @@ public class Crafter : MonoBehaviour
 
     // Dico
     Dictionary<string, string> dicoFood = new Dictionary<string, string>();
+    Dictionary<string, Image> dicoImage = new Dictionary<string , Image>();
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Init dictionnaire des plats
-        // Bac 1
-        dicoFood.Add("riztomate",               "salade");
-        dicoFood.Add("surimitomate",            "salade");
-        dicoFood.Add("farineoeuf",              "crepes");
-        dicoFood.Add("eaufarine",               "pain");
-        dicoFood.Add("pommesucre",              "pommeaufour");
-        // Bac 2
-        dicoFood.Add("patatefromage",           "raclette");
-        dicoFood.Add("oeuffromage",             "omelettefromage");
-        dicoFood.Add("rizlait",                 "rizaulait");
-        dicoFood.Add("patesjambon",             "patesjambon");
-        dicoFood.Add("patatechou",              "poeleedepatates");
-        dicoFood.Add("tomatejambon",            "tomatesfarcies");
-        dicoFood.Add("patatesteak",             "steakfrites");
-        dicoFood.Add("patessteak",              "patesbolognaises");
-        dicoFood.Add("steakoeuf",               "tartare");
-        dicoFood.Add("rizpoisson",              "sushi");
-        // Bac 3
-        dicoFood.Add("eaubonbon",               "maoambubbletea");
-        dicoFood.Add("choufleur",               "choufleur");
-        dicoFood.Add("poissonchips",            "fishandchips");
-        dicoFood.Add("poissonchocolat",         "lefisheauchocolat");
-        dicoFood.Add("tabacsurimi",             "surispliff");
-        dicoFood.Add("feubonbon",               "chamallowgrille");
-        dicoFood.Add("laitcereal",              "ptitdej");
-        dicoFood.Add("feualcool",               "cocktailmolotov");
-        dicoFood.Add("orangechocolat",          "gouter");
-        dicoFood.Add("beurrechocolat",          "painauchocolat");
-        dicoFood.Add("sucrefeu",                "caramel");
-        dicoFood.Add("boisalcool",              "tonneauderhum");
-        dicoFood.Add("tissutabac",              "cigarette");
-        dicoFood.Add("tomatetissu",             "torchontache");
-        dicoFood.Add("saladefeu",               "lanceroquettes");
-        // Bac 4
-        dicoFood.Add("flinguefromage",          "gratindeflingue");
-        dicoFood.Add("flingueeau",              "soupedeflingue");
-        dicoFood.Add("pommechausson",           "chaussonauxpommes");
-        dicoFood.Add("oiseausansailes",         "lefeur");
-        dicoFood.Add("arsenicfarine",           "puddingalarsenic");
-        dicoFood.Add("canardlaque",             "canardlaque");
-        dicoFood.Add("tabacargent",             "snoopdogg");
-        dicoFood.Add("krakensalade",            "saladedekraken");
-        dicoFood.Add("jokerorange",             "jusdorange");
-        dicoFood.Add("consolejoker",            "batman");
-        dicoFood.Add("bonbonconsole",           "kirby");
-        dicoFood.Add("inimoisiarsenic",         "unknown");
-        dicoFood.Add("canardoiseausansailes",   "poussin");
-        dicoFood.Add("chipsconsole",            "soireegaming");
-        dicoFood.Add("krakenbeurre",            "krakenaubeurre");
-        dicoFood.Add("krakenfarine",            "cracotte");
-        dicoFood.Add("alcoolkraken",            "lekraken");
-        dicoFood.Add("inimoisiinimoisi",        "boitedepandore");
+        this.initRecettes();
+
 
         foodPicture.gameObject.SetActive(false);
     }
-    
+
+    private void initRecettes()
+    {
+        // Init dictionnaire des plats
+        // Bac 1
+        dicoFood.Add("riztomate", "salade");
+        dicoFood.Add("surimitomate", "salade");
+        dicoFood.Add("farineoeuf", "crepes");
+        dicoFood.Add("eaufarine", "pain");
+        dicoFood.Add("pommesucre", "pommeaufour");
+        // Bac 2
+        dicoFood.Add("patatefromage", "raclette");
+        dicoFood.Add("oeuffromage", "omelettefromage");
+        dicoFood.Add("rizlait", "rizaulait");
+        dicoFood.Add("patesjambon", "patesjambon");
+        dicoFood.Add("patatechou", "poeleedepatates");
+        dicoFood.Add("tomatejambon", "tomatesfarcies");
+        dicoFood.Add("patatesteak", "steakfrites");
+        dicoFood.Add("patessteak", "patesbolognaises");
+        dicoFood.Add("steakoeuf", "tartare");
+        dicoFood.Add("rizpoisson", "sushi");
+        // Bac 3
+        dicoFood.Add("eaubonbon", "maoambubbletea");
+        dicoFood.Add("choufleur", "choufleur");
+        dicoFood.Add("poissonchips", "fishandchips");
+        dicoFood.Add("poissonchocolat", "lefisheauchocolat");
+        dicoFood.Add("tabacsurimi", "surispliff");
+        dicoFood.Add("feubonbon", "chamallowgrille");
+        dicoFood.Add("laitcereal", "ptitdej");
+        dicoFood.Add("feualcool", "cocktailmolotov");
+        dicoFood.Add("orangechocolat", "gouter");
+        dicoFood.Add("beurrechocolat", "painauchocolat");
+        dicoFood.Add("sucrefeu", "caramel");
+        dicoFood.Add("boisalcool", "tonneauderhum");
+        dicoFood.Add("tissutabac", "cigarette");
+        dicoFood.Add("tomatetissu", "torchontache");
+        dicoFood.Add("saladefeu", "lanceroquettes");
+        // Bac 4
+        dicoFood.Add("flinguefromage", "gratindeflingue");
+        dicoFood.Add("flingueeau", "soupedeflingue");
+        dicoFood.Add("pommechausson", "chaussonauxpommes");
+        dicoFood.Add("oiseausansailes", "lefeur");
+        dicoFood.Add("arsenicfarine", "puddingalarsenic");
+        dicoFood.Add("canardlaque", "canardlaque");
+        dicoFood.Add("tabacargent", "snoopdogg");
+        dicoFood.Add("krakensalade", "saladedekraken");
+        dicoFood.Add("jokerorange", "jusdorange");
+        dicoFood.Add("consolejoker", "batman");
+        dicoFood.Add("bonbonconsole", "kirby");
+        dicoFood.Add("inimoisiarsenic", "unknown");
+        dicoFood.Add("canardoiseausansailes", "poussin");
+        dicoFood.Add("chipsconsole", "soireegaming");
+        dicoFood.Add("krakenbeurre", "krakenaubeurre");
+        dicoFood.Add("krakenfarine", "cracotte");
+        dicoFood.Add("alcoolkraken", "lekraken");
+        dicoFood.Add("inimoisiinimoisi", "boitedepandore");
+    }
+    /*
+    private void initFoodImages()
+    {
+        dicoImage.Add("crepe", foodPicture);
+    }
+    */
 
     public void AddIngredient(string ingredient)
     {
@@ -135,6 +148,7 @@ public class Crafter : MonoBehaviour
         foodPicture.gameObject.SetActive(true);
 
         Debug.Log("Entre coroutine");
+        yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         Debug.Log("Got Input");
 
